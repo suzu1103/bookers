753d05fb32a2c86212e9a87a19eb57bb5dece46f
suzu1103 <4425krkh@gmail.com>
2022-03-31 14:32:57 +0000

    [Add]3-12 編集、更新、削除　追加

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -2,7 +2,7 @@ class BooksController < ApplicationController
   def new
     @book = Book.new
   end
-  
+
   def create
     book = Book.new(book_params)
     book.save
@@ -20,10 +20,23 @@ class BooksController < ApplicationController
   def edit
     @book = Book.find(params[:id])
   end
-  
+
+  def update
+    book = Book.find(params[:id])
+    book.update(book_params)
+    redirect_to book_path(book.id)
+  end
+
+  def destroy
+    book = Book.find(params[:id])
+    book.destroy
+    redirect_to books_path
+  end
+
   private
+  
   def book_params
     params.require(:book).permit(:title, :body)
   end
-  
+
 end
