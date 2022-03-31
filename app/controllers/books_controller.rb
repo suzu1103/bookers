9790f4eb2057b548cc8f3150e46abcbb70de5504
suzu1103 <4425krkh@gmail.com>
2022-03-31 13:36:29 +0000

    [Add]3-12 一覧、詳細ページの追加

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -6,17 +6,19 @@ class BooksController < ApplicationController
   def create
     book = Book.new(book_params)
     book.save
-    redirect_to '/top'
-    # root
+    redirect_to book_path(book.id)
   end
 
   def index
+    @books = Book.all
   end
 
   def show
+    @book = Book.find(params[:id])
   end
 
   def edit
+    @book = Book.find(params[:id])
   end
   
   private
